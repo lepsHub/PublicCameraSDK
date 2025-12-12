646d2571636c6d2f5e15852653ebb9636b66e9ac
lepsHub <backup.user.leps@outlook.com>
2025-12-12 17:56:20 +0000

    SPM: Update Package.swift for 78.2.8-BETA.5

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@
     targets: [
       .binaryTarget(
         name: "CameraSDK",
-        url: "https://github.com/lepsHub/PublicCameraSDK/releases/download/78.2.6/CameraSDK.xcframework.zip",
-        checksum: "3d29997239e658377880681ecb1b1399d87c102e324bc10baad46833267fc700"
+        url: "https://github.com/lepsHub/PublicCameraSDK/releases/download/78.2.8-BETA.5/CameraSDK.xcframework.zip",
+        checksum: "ddcd3070a444d0a0d758902284b58ae6d3d9a3ae0d1c163c7e6777aacab6ab3e"
       ),
       .target(
         name: "CameraSDKTargets",
