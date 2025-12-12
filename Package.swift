93ffb0d667ad901e53059caa4ac27071b53ed16c
lepsHub <backup.user.leps@outlook.com>
2025-12-12 06:24:16 +0000

    SPM: Update Package.swift for 78.2.3-BETA.12

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.8
+
+  import PackageDescription
+              
+  let package = Package(
+    name: "CameraSDK",
+    products: [
+      .library(name: "CameraSDK", targets: ["CameraSDKTargets"])
+    ],
+    targets: [
+      .binaryTarget(
+        name: "CameraSDK",
+        url: "https://github.com/lepsHub/PublicCameraSDK/releases/download/78.2.3-BETA.12/CameraSDK.xcframework.zip",
+        checksum: ""
+      ),
+      .target(name: "CameraSDKTargets", dependencies: ["CameraSDK"], path: "Sources")
+    ]
+  )
+  
